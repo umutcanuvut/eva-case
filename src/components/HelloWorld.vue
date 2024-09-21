99320db26661a657ef2e2717690408a705919de8
umutcanuvut <umutcanuvut@gmail.com>
2024-09-21 18:32:32 +0300

    initial project setup witchore: set up project with Vite, Vue 3, TypeScript, Tailwind CSS, Prettier, and Shadcn UI h

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <Button class="bg-red-500 text-center text-white">Click me</Button>
+</template>
